428eff3540dc5696976a8b916e8b84cbe5acaca3
devThiagoJorge <thiago.v.jorge@hotmail.com>
2024-05-30 01:31:47 -0300

    Added validations to models

---
+++ ApiTresCamadas/src/DevIo.Domain/Models/Validations/EnderecoValidation.cs
@@ -0,0 +1,44 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIo.Domain.Models.Validations
+{
+    public class EnderecoValidation : AbstractValidator<Endereco>
+    {
+        public EnderecoValidation()
+        {
+            RuleFor(c => c.Logradouro)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(2, 200).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres.");
+
+            RuleFor(c => c.Bairro)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(2, 100).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres.");
+
+            RuleFor(c => c.Cep)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(2, 8).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres.");
+
+            RuleFor(c => c.Cep)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(2, 8).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres.");
+
+            RuleFor(c => c.Cidade)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(2, 120).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres.");
+
+            RuleFor(c => c.Estado)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(2, 100).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres.");
+
+            RuleFor(c => c.Numero)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(2, 10).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres.");
+
+        }
+    }
+}
---
+++ ApiTresCamadas/src/DevIo.Domain/Models/Validations/FornecedorValidation.cs
@@ -0,0 +1,32 @@
+﻿using DevIo.Domain.Models.Validations.DocumentosValidations;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIo.Domain.Models.Validations
+{
+    public class FornecedorValidation : AbstractValidator<Fornecedor>
+    {
+        public FornecedorValidation()
+        {
+            RuleFor(c => c.Nome)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .Length(2, 200).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres.");
+
+            When(c => c.TipoFornecedor == TipoFornecedor.PessoaFisica, () =>
+            {
+                RuleFor(c => CpfCnpjUtils.IsCpf(c.Documento))
+                .Equal(true).WithMessage("O documento fornecido é inválido.");
+            });
+
+            When(c => c.TipoFornecedor == TipoFornecedor.PessoaJuridica, () =>
+            {
+                RuleFor(c => CpfCnpjUtils.IsCnpj(c.Documento))
+                .Equal(true).WithMessage("O documento fornecido é inválido.");
+            });
+        }
+    }
+}

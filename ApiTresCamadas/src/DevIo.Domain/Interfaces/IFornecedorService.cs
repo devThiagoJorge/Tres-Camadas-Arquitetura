60bf893c059a50914ba7f8f6e63bfadb4ac56996
devThiagoJorge <thiago.v.jorge@hotmail.com>
2024-05-31 22:55:46 -0300

    Added interfaces of sevices

---
+++ ApiTresCamadas/src/DevIo.Domain/Interfaces/IFornecedorService.cs
@@ -0,0 +1,16 @@
+﻿using DevIo.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIo.Domain.Interfaces
+{
+    public interface IFornecedorService : IDisposable
+    {
+        Task Adicionar(Fornecedor fornecedor);
+        Task Atualizar(Fornecedor fornecedor);
+        Task Remover(Guid id);
+    }
+}
---
+++ ApiTresCamadas/src/DevIo.Domain/Interfaces/IProdutoService.cs
@@ -0,0 +1,11 @@
+﻿using DevIo.Domain.Models;
+
+namespace DevIo.Domain.Interfaces
+{
+    public interface IProdutoService : IDisposable
+    {
+        Task Adicionar(Produto fornecedor);
+        Task Atualizar(Produto fornecedor);
+        Task Remover(Guid id);
+    }
+}

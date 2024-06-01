5276159a386004b837a41da95a2bb5c3ea5b0224
devThiagoJorge <thiago.v.jorge@hotmail.com>
2024-05-31 23:13:00 -0300

    Added interfaces of the Repository Pattern

---
+++ ApiTresCamadas/src/DevIo.Domain/Interfaces/IFornecedorRepository.cs
@@ -0,0 +1,16 @@
+﻿using DevIo.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIo.Domain.Interfaces
+{
+    public interface IFornecedorRepository : IRepository<Fornecedor>
+    {
+        Task<Fornecedor> ObterFornecedorEndereco(Guid id);
+        Task<Fornecedor> ObterFornecedorProdutosEndereco(Guid id);
+        Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId);
+    }
+}

c185db75a3ea8ed83eae8df9d880301a21823ad2
devThiagoJorge <thiago.v.jorge@hotmail.com>
2024-06-05 23:56:26 -0300

    Created DbContext and Project API, Injection Dependency in Db and migrations!

--- ApiTresCamadas/src/DevIO.Data/Context/AppDbContext.cs
+++ ApiTresCamadas/src/DevIO.Data/Context/AppDbContext.cs
@@ -13,6 +13,9 @@ namespace DevIO.Data.Context
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking; // remove o tracking do objeto;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+
 
         }
 
@@ -26,11 +29,30 @@ namespace DevIO.Data.Context
                 .SelectMany(e => e.GetProperties()
                 .Where(p => p.ClrType == typeof(string))))
                 property.SetColumnType("varchar(100)");
-           
+
 
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
 
             base.OnModelCreating(modelBuilder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var item in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if(item.State == EntityState.Added)
+                {
+                    item.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+
+                if(item.State == EntityState.Modified)
+                {
+                    item.Property("DataCadastro").IsModified = false;
+
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
         }
     }
 }

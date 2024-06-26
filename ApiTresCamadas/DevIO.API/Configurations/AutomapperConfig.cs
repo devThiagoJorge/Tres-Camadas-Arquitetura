2e7c25cfbf53e47569565440a8700de273368611
devThiagoJorge <thiago.v.jorge@hotmail.com>
2024-06-25 23:04:48 -0300

    Add Automapper

---
+++ ApiTresCamadas/DevIO.API/Configurations/AutomapperConfig.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using DevIo.Domain.Models;
+using DevIO.API.DTO;
+
+namespace DevIO.API.Configurations
+{
+    public class AutomapperConfig : Profile
+    {
+        public AutomapperConfig()
+        {
+            CreateMap<Fornecedor, FornecedorDto>().ReverseMap();
+            CreateMap<Endereco, EnderecoDto>().ReverseMap();
+            CreateMap<ProdutoDto, Produto>();
+
+
+            CreateMap<Produto, ProdutoDto>()
+                .ForMember(dest => dest.NomeFornecedor,
+                opt => opt.MapFrom(src => src.Fornecedor.Nome));
+        }
+    }
+}
--- ApiTresCamadas/DevIO.API/Program.cs
+++ ApiTresCamadas/DevIO.API/Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

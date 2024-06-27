faba5f65c34f529bd47e5f069fb5d2cbce7c59c3
devThiagoJorge <thiago.v.jorge@hotmail.com>
2024-06-26 22:18:15 -0300

    Update ProdutoController

--- ApiTresCamadas/DevIO.API/Controllers/MainController.cs
+++ ApiTresCamadas/DevIO.API/Controllers/MainController.cs
@@ -1,9 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace DevIO.API.Controllers
 {
     [ApiController]
     public abstract class MainController : ControllerBase
     {
+        protected bool OperacaoValida()
+        {
+            return true;
+        }
+
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (OperacaoValida())
+            {
+                return new ObjectResult(result);
+            }
+
+            return BadRequest(new
+            {
+
+            });
+        }
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            if(!modelState.IsValid) { }
+
+            return CustomResponse();
+        }
+
+        protected void NotificaErro(string mensagem)
+        {
+
+        }
     }
 }
--- ApiTresCamadas/DevIO.API/Controllers/ProdutosController.cs
+++ ApiTresCamadas/DevIO.API/Controllers/ProdutosController.cs
@@ -1,15 +1,96 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using AutoMapper;
+using DevIo.Domain.Interfaces;
+using DevIo.Domain.Models;
+using DevIO.API.DTO;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DevIO.API.Controllers
 {
     [Route("api/produtos")]
     public class ProdutosController : MainController
     {
-        public ProdutosController()
+        private readonly IProdutoRepository _produtoRepository;
+        private readonly IProdutoService _produtoService;
+        private readonly IMapper _mapper;
+
+        public ProdutosController(IProdutoService produtoService,
+            IProdutoRepository produtoRepository,
+            IMapper mapper)
         {
-          
+            _produtoService = produtoService;
+            _produtoRepository = produtoRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ProdutoDto>> ObterTodos() =>
+            _mapper.Map<IEnumerable<ProdutoDto>>(await _produtoRepository.ObterProdutosFornecedores());
+
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<ProdutoDto>> ObterPorId(Guid id)
+        {
+            var produtoViewModel = await ObterProdutoPorId(id);
+
+            if (produtoViewModel == null)
+                return NotFound();
+
+            return produtoViewModel;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProdutoDto>> Adicionar(ProdutoDto produtoDto)
+        {
+            if (!ModelState.IsValid)
+                return CustomResponse(ModelState);
+
+            await _produtoService.Adicionar(_mapper.Map<Produto>(produtoDto));
+
+            return CustomResponse(produtoDto);
+
+        }
+
+        [HttpPut("id:guid")]
+        public async Task<IActionResult> Atualizar(Guid id, ProdutoDto produtoDto)
+        {
+            if (id != produtoDto.Id)
+            {
+                NotificaErro("Os ids informados não são iguais.");
+                return CustomResponse();
+            }
+
+            var produtoAtualizacao = await ObterProdutoPorId(id);
+
+            produtoAtualizacao.FornecedorId = produtoDto.FornecedorId;
+            produtoAtualizacao.Nome = produtoDto.Nome;
+            produtoAtualizacao.Descricao = produtoDto.Descricao;
+            produtoAtualizacao.Valor = produtoDto.Valor;
+            produtoAtualizacao.Ativo = produtoDto.Ativo;
+
+            await _produtoService.Atualizar(_mapper.Map<Produto>(produtoAtualizacao));
+            return CustomResponse();
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<ProdutoDto>> Excluir(Guid id)
+        {
+            var produto = await ObterProdutoPorId(id);
+
+            if (produto == null)
+                return NotFound();
+
+            await _produtoService.Remover(id);
+
+            return CustomResponse();
         }
 
 
+        private async Task<ProdutoDto> ObterProdutoPorId(Guid id)
+        {
+            return _mapper.Map<ProdutoDto>(await _produtoRepository.ObterProdutoFornecedor(id));
+        }
     }
+
 }
+
